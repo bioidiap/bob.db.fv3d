b46a3a10de6fdffe39fc0a786bd9dab9b4c3475d
Andre Anjos <andre.anjos@idiap.ch>
2017-10-11 14:35:40 +0200

    Remove extra import

--- bob/db/fv3d/driver.py
+++ bob/db/fv3d/driver.py
@@ -74,7 +74,6 @@ class Interface(BaseInterface):
 
 
   def version(self):
-    import pkg_resources  # part of setuptools
     return pkg_resources.require('bob.db.%s' % self.name())[0].version
 
 
